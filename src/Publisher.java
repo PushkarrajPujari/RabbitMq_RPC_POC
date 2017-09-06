daa1e6cc982da64af583f9d33ea4c9bfdd50894f
Pushkarraj <user13@sunrichtech.com>
2017-09-06 10:12:27 +0530

    Sanity Check

---
+++ src/Publisher.java
@@ -0,0 +1,12 @@
+/**
+ * @Author : Pushkarraj pujari
+ * @Since : 06/09/2017
+ */
+public class Publisher {
+    /**
+     * Sanity Check
+     * */
+    public static void main(String[] args) {
+        System.out.println("Welcome to RPC");
+    }
+}
